4d1df6759027360f8cd4c33d90bacb14eca0fc15
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-07-28 17:47:06 +0200

    Port remaining KCMs away from keyword & KServiceTypeTrader based plugin loading
    Task: https://phabricator.kde.org/T14517
    Task: https://phabricator.kde.org/T14744

---
+++ Modules/info/kcm_interrupts.cpp
@@ -0,0 +1,10 @@
+/*
+   SPDX-FileCopyrightText: 2021 Alexander Lohnau <alexander.lohnau@gmx.de>
+   SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
+#include "info.h"
+
+CREATE_FACTORY(IRQ, i18n("Interrupt"), "interrupts.json")
+
+#include "kcm_interrupts.moc"
