0cc74752c7bacd33d9aed4c1390ce477d9106460
l10n daemon script <scripty@kde.org>
2023-01-20 02:22:06 +0000

    Add an ExtraData section to kcm-about-distrorc
    When item is present, run the given scripts if possible and add the
    values to the gui under their respective names.

---
+++ Modules/about-distro/src/ThirdPartyEntry.cpp
@@ -0,0 +1,60 @@
+/*
+    SPDX-FileCopyrightText: 2023 Jeremy Whiting <jeremy.whiting@collabora.com>
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
+#include "ThirdPartyEntry.h"
+
+#include <QDebug>
+#include <QProcess>
+
+QStringList getCommandOutput(const QString &command, const QString &lang = QString())
+{
+    auto process = new QProcess(nullptr);
+    if (!lang.isEmpty()) {
+        QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+        env.insert("LANGUAGE", lang); // Add an environment variable
+        process->setProcessEnvironment(env);
+    }
+
+    process->start(command, QStringList());
+    process->waitForFinished();
+    return QString(process->readAllStandardOutput()).split('\n');
+}
+
+ThirdPartyEntry::ThirdPartyEntry(const QString &scriptPath)
+    : Entry(ki18nc("Unused but needs to be : to avoid assertion in Entry constructor", ":"), QString())
+    , m_scriptPath(scriptPath)
+{
+    // Get default and value
+    QStringList output = getCommandOutput(m_scriptPath);
+    processOutput(output, Language::System);
+
+    QStringList englishOutput = getCommandOutput(m_scriptPath, "en_US:C");
+    processOutput(englishOutput, Language::English);
+}
+
+QString ThirdPartyEntry::localizedLabel(Language language) const
+{
+    return m_labels[language];
+}
+
+QString ThirdPartyEntry::localizedValue(Language language) const
+{
+    return m_values[language];
+}
+
+void ThirdPartyEntry::processOutput(const QStringList &output, Language language)
+{
+    for (const auto &line : output) {
+        if (!line.isEmpty()) {
+            const QStringList words = line.split(QLatin1Char('\t'));
+            if (words.size() < 2) {
+                qWarning() << "Script: " << m_scriptPath << " missing 2 tab separated values, ignoring: " << line;
+            } else {
+                m_labels[language] = words.at(0);
+                m_values[language] = words.at(1);
+            }
+        }
+    }
+}
