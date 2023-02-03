7207b460bbb865ded52df82fad74129d55f2d36c
Jeremy Whiting <jpwhiting@kde.org>
2023-02-03 10:28:23 -0700

    Add an ExtraSoftwareData section to kcm-about-distrorc
    When item is present, run the given scripts if possible and add the
    values to the gui under their respective names.

--- Modules/about-distro/example/kcm-about-distrorc.ex
+++ Modules/about-distro/example/kcm-about-distrorc.ex
@@ -6,3 +6,4 @@ Website=http://www.kubuntu.org
 Version=1.2.3
 Variant=Unstable Branches
 UseOSReleaseVersion=true # controls if os-release's VERSION is used (default=false = use VERSION_ID)
+ExtraSoftwareData=/usr/bin/myscript;/usr/bin/myotherscript # Scripts to run to get extra software data to show
