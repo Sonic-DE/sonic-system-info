4d1a4984d46ba8705f2b92951e5c19164bbc0323
Oliver Beard <olib141@outlook.com>
2025-02-20 22:43:47 +0000

    kcms/about-distro: Add help property to Entry & show total amount of installed memory in MemoryEntry This provides additional information to the user, with a new help tooltip that clarifies the displayed values as is contextually appropriate. For example, if the shown message is "32 GB of RAM (31.3 GB usable)", the tooltip will elucidate that some memory is reserved for use by system hardware. BUG: 500412

--- kcms/about-distro/src/MemoryEntry.cpp
+++ kcms/about-distro/src/MemoryEntry.cpp
@@ -9,6 +9,9 @@
 
 #ifdef Q_OS_LINUX
 #include <sys/sysinfo.h>
+#ifdef UDEV_FOUND
+#include <libudev.h>
+#endif
 #elif defined(Q_OS_FREEBSD)
 // clang-format off
 #include <sys/types.h>
@@ -19,9 +22,54 @@
 MemoryEntry::MemoryEntry()
     : Entry(ki18n("Memory:"), QString() /* overridden here */)
 {
+    m_totalRam = calculateTotalRam();
+    m_availableRam = calculateAvailableRam();
 }
 
 qlonglong MemoryEntry::calculateTotalRam()
+{
+    qlonglong ret = -1;
+
+#if defined(Q_OS_LINUX) && defined(UDEV_FOUND)
+    struct udev *udev = udev_new();
+    if (!udev) {
+        return ret;
+    }
+
+    struct udev_device *dmi = udev_device_new_from_syspath(udev, "/sys/devices/virtual/dmi/id");
+    if (!dmi) {
+        return ret;
+    }
+
+    const char *num_memory_devices_cstr = udev_device_get_property_value(dmi, "MEMORY_ARRAY_NUM_DEVICES");
+    if (!num_memory_devices_cstr) {
+        return ret;
+    }
+
+    ret = 0;
+    int num_memory_devices = atoi(num_memory_devices_cstr);
+    for (int i = 0; i < num_memory_devices; ++i) {
+        const char *memory_bytes_cstr = udev_device_get_property_value(dmi, QStringLiteral("MEMORY_DEVICE_%1_SIZE").arg(i).toLatin1());
+        if (memory_bytes_cstr) {
+            ret += strtoull(memory_bytes_cstr, nullptr, 10);
+        }
+    }
+
+    udev_device_unref(dmi);
+    udev_unref(udev);
+#endif
+
+    /*
+     * TODO: A FreeBSD impl is likely possible, as sysctlbyname() can be used
+     *         with "hw.physmem", "hw.usermem" and "hw.realmem".
+     *       We should be careful that both this and calculateAvailableRAM
+     *         return what is actually wanted!
+     */
+
+    return ret;
+}
+
+qlonglong MemoryEntry::calculateAvailableRam()
 {
     qlonglong ret = -1;
 #ifdef Q_OS_LINUX
@@ -44,11 +92,53 @@ qlonglong MemoryEntry::calculateTotalRam()
 
 QString MemoryEntry::localizedValue(Language language) const
 {
-    const qlonglong totalRam = calculateTotalRam();
-    if (totalRam > 0) {
-        const auto string = ki18nc("@label %1 is the formatted amount of system memory (e.g. 7,7 GiB)", "%1 of RAM")
-                                .subs(KFormat(localeForLanguage(language)).formatByteSize(totalRam));
+    const int totalRamPrecision = (m_totalRam % (1024 * 1024 * 1024) == 0) ? 0 : 1;
+    const int availableRamPrecision = (m_availableRam % (1024 * 1024 * 1024) == 0) ? 0 : 1;
+
+    if (m_totalRam >= 0 && m_availableRam >= 0) {
+        // Both known
+        const auto string = ki18nc("@label, %1 is the total amount of installed system memory, %2 is the amount of which is usable, both expressed as 7.7 GiB",
+                                   "%1 of RAM (%2 usable)")
+                                .subs(KFormat(localeForLanguage(language)).formatByteSize(m_totalRam, totalRamPrecision))
+                                .subs(KFormat(localeForLanguage(language)).formatByteSize(m_availableRam, availableRamPrecision));
         return localize(string, language);
+    } else if (m_totalRam >= 0 && m_availableRam < 0) {
+        // Known total, unknown available
+        const auto string = ki18nc("@label, %1 is the amount of installed system memory expressed as 7.7 GiB", "%1 of RAM")
+                                .subs(KFormat(localeForLanguage(language)).formatByteSize(m_totalRam, totalRamPrecision));
+        return localize(string, language);
+    } else if (m_totalRam < 0 && m_availableRam >= 0) {
+        // Unknown total, known available
+        const auto string = ki18nc("@label, %1 is the amount of usable system memory expressed as 7.7 GiB", "%1 of usable RAM")
+                                .subs(KFormat(localeForLanguage(language)).formatByteSize(m_availableRam, availableRamPrecision));
+        return localize(string, language);
+    } else {
+        // Both unknown
+        return localize(ki18nc("@label, Unknown amount of system memory", "Unknown"), language);
     }
-    return localize(ki18nc("Unknown amount of RAM", "Unknown"), language);
+}
+
+QString MemoryEntry::localizedHelp(Language language) const
+{
+    if (m_totalRam >= 0 && m_availableRam >= 0) {
+        // Both known
+        return localize(ki18nc("@info:tooltip, referring to system memory or RAM",
+                               "Some memory is reserved for use by system hardware, such as integrated graphics memory."),
+                        language);
+    } else if (m_totalRam >= 0 && m_availableRam < 0) {
+        // Known total, unknown available
+        return localize(ki18nc("@info:tooltip, referring to system memory or RAM",
+                               "The amount of usable memory may be lower than the displayed amount because some memory is reserved for use by system hardware, "
+                               "such as integrated graphics memory."),
+                        language);
+    } else if (m_totalRam < 0 && m_availableRam >= 0) {
+        // Unknown total, known available
+        return localize(ki18nc("@info:tooltip, referring to system memory or RAM",
+                               "The amount of memory displayed may be lower than the installed amount because some memory is reserved for use by system "
+                               "hardware, such as integrated graphics memory."),
+                        language);
+    } else {
+        // Both unknown
+        return QString();
+    }
 }
